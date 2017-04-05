9f5f499fa88abdc2169b843adf5196e3e8d16ccc
gromero2095 <gersonromero2095@gmail.com>
2017-04-05 03:29:48 -0500

    Actualizacion 05/04/2017 3:29

---
+++ BankingSystem/CentralTelefonica.Entities/Class2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralTelefonica.Entities
+{
+    class Class2
+    {
+        hola
+    }
+}
